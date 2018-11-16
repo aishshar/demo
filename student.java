f9db00101e28bce16b5d83f40598949639cda280
aishshar <43337053+aishshar@users.noreply.github.com>
2018-11-16 10:41:50 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
- 
 class ChkPalindrome
 {
    public static void main(String args[])
    {
    
       if (str.equals(rev))
-         System.out.println(str+" is a palindrome");
+         
       else
-         System.out.println(str+" is not a palindrome");
+        
  
    }
 }
